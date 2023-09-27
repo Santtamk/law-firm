4645ccb7ff93b7c8e1a22bea2ace503e79af4841
Santam Kumai <santtamk@gmail.com>
2023-09-27 23:42:53 +0530

    last updates is Faq

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import HeroSection from './Components/HeroSection/HeroSection';
 import Introduction from './Components/Introduction/Introduction';
 import Navbar from './Components/NavBar/Navbar';
+import Practices from './Components/Practices/Practices';
+import WhyUs from './Components/WhyUs/WhyUs';
+import HappyClients from './Components/HappyClients/HappyClients'
+import { Team } from './Components/Team/Team';
+import Faq from './Components/FAQ/Faq';
 
 function App() {
   return (
@@ -9,13 +14,13 @@ function App() {
       <Navbar />
       <HeroSection />
       <Introduction />
+      <WhyUs />
+      <Practices />
+      <HappyClients />
+      <Team />
+      <Faq />
 
     </>
-    // <WhyChooseUs />
-    // <AreaOfPractice />
-    // <HappyClients />
-    // <Team/>
-    // <FAQ />
     // <Newsletter />
     // <Footer />
   );
